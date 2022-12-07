9c1b2db1f10bedbe04927949c578d763d4adbbe4
Aksellofstedtnielsen <aksel98@outlook.com>
2022-12-07 01:14:37 +0100

    Fixing

--- TicTacToe/StartViewController.swift
+++ TicTacToe/StartViewController.swift
@@ -23,10 +23,6 @@ class StartViewController: UIViewController {
         
         
     }
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
 
     @IBAction func segmentClick(_ sender: Any) {
         switch segmentControllOutlet.selectedSegmentIndex{
@@ -59,10 +55,7 @@ class StartViewController: UIViewController {
     }
 
 
-       
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    
+ 
    
 
 
